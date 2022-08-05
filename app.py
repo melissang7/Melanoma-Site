067de3270bd9ee214cd77e4933a1a76ebbd305a6
Melissa Nogueira <102537724+melissang7@users.noreply.github.com>
2022-08-05 17:32:16 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#criar a 1° página do site
+#route -> melanoma.com.br/
+#função -> o que será exibido na página.
+
+@app.route("/")  #atribuir uma nova funcionalidade para a função abaixo
+def DiagnosticoMelanoma():
+    return render_template('homepage.HTML')
+
+@app.route("/final")
+def final():
+    return render_template ("DiagnosticoFinal.HTML")
+#colocar o site no ar
+if __name__ == "__main__":   #executar o código quando rodar diretamente
+    app.run(debug=True)
+ 
